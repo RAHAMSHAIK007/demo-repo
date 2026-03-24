192dd5b36fbc535e334f462f51421cc91deeb306
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2026-03-24 06:41:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  ami           = "ami-02dfbd4ff395f2a1b"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "dev-server-gab"
+  }
+}
